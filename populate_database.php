922b2eda76f746d695c38f34f9a40e287380da87
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2012-01-09 15:20:59 -0500

    update popuplate_database to use new database schema

--- populate_database.php
+++ populate_database.php
@@ -2,20 +2,15 @@
 // Scrapes Metafilter.com and pushes stuff into the database.
 
 $web = isset($_SERVER['SERVER_PROTOCOL']) &&
-       $_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1';
+			 $_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1';
 $break = $web ? '<br/>' : "\n";
 $split = $web ? '<hr/>' : "-----\n";
+
+include('functions.php');
 include("connect_mysql.php");
-if ($_REQUEST['tables']) {
-	define("ENTRIES_TABLE", "hyperlinks_entries_" . $_REQUEST['tables']);
-	define("LINKS_TABLE", "hyperlinks_links_" . $_REQUEST['tables']);
-} else if ($_ENV['CONSTITUENCY_TABLES']) {
-	define("ENTRIES_TABLE", "hyperlinks_entries_" . $_ENV['CONSTITUENCY_TABLES']);
-	define("LINKS_TABLE", "hyperlinks_links_" . $_ENV['CONSTITUENCY_TABLES']);
-} else {
-	define("ENTRIES_TABLE", "hyperlinks_entries");
-	define("LINKS_TABLE", "hyperlinks_links");
-}
+
+define("ENTRIES_TABLE", "entries");
+define("LINKS_TABLE", "links");
 
 $start = 1;
 $end = 10;
@@ -28,46 +23,45 @@ if (isset($_GET['end']))
 if (isset($_SERVER['argv'][2]))
 	$end = $_SERVER['argv'][2];
 
-for($id = $start; $id < $end; $id++) {
+for ($id = $start; $id < $end; $id++) {
 	echo "$id{$break}";
 	
 	// Pull out all of the data, if any
 	$data = getEntryData(getEntry($id));
 
 	if (sizeof($data) != 3) {
-		echo "\thas no data, size = " . strval(sizeof($data)) . $break;
-	} else {
-		echo "\thas data{$break}";
-		
-		// Push entry into the database
-		$author = mysql_real_escape_string(trim($data[2]));
-		$content = mysql_real_escape_string(trim($data[1]));
-
-		mysql_query("INSERT INTO " . ENTRIES_TABLE . " (id,author,content,modified_by) VALUES ($id,{$author},\"{$content}\",'$user') on duplicate key update author = {$author}, content = \"{$content}\", processed = NULL");
-		
-		// Pull out the links
-		$linkData = getLinkData($data[1]);
-		$linkValues = array();
-		
-		$numberofLinks = sizeof($linkData);
-		
-    for ($lid = 0; $lid < $numberofLinks; $lid++) {
-			$link = $linkData[$lid];
-			
-			$entry = mysql_real_escape_string($link[1]);
-			$href = mysql_real_escape_string($link[2]);
-			
-			array_push($linkValues, "($lid,$id,\"{$entry}\",\"{$href}\")");
-		}
-    	
-		echo "\thas $lid links{$break}";
-		
-		if ($lid > 0) {
-			mysql_query("INSERT INTO " . LINKS_TABLE . " (id,entry,href,text) VALUES " . implode(",", $linkValues));
-		}
+		echo "\thas no data, size = " . strval(sizeof($data)) . $break . $split;
+		continue;
 	}
 	
-	echo $split;
+	// Check if it's already in the database, skip.
+	if ( $db->get_var('select id from ' . ENTRIES_TABLE . " where id = $id") ) {
+		echo "\talready in database, skipping" . $break . $split;
+		continue;
+	}
+	
+	echo "\thas data{$break}";
+		
+	// Push entry into the database
+	$db->insert(ENTRIES_TABLE, array(
+		'id' => (int) $id,
+		'author' => (int) $data[2],
+		'content' => $data[1]
+	), array('%d', '%d', '%s'));
+
+	// Pull out the links
+	$linkData = getLinkData($data[1]);
+	foreach ( $linkData as $lid => $link ) {
+		$link = $linkData[$lid];
+		$db->insert(LINKS_TABLE, array(
+			'id' => $lid,
+			'entry' => $id,
+			'href' => $link[1],
+			'text' => $link[2]
+		), array('%d', '%d', '%s', '%s'));
+	}
+	
+	echo "\thas " . count($linkData) . " links{$break}{$split}";
 }
 
 /**
