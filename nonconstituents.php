97ec579c483eaa0f7f12a2c7c1b06352b1594624
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2012-03-13 23:15:27 -0400

    nonconstituents: + cause labels

--- nonconstituents.php
+++ nonconstituents.php
@@ -18,6 +18,8 @@ else
 
 <div class="container" id='container'>
 <?php
+$tags = $db->get_results("select * from tags", OBJECT_K);
+
 // @todo can't I just order and then group to get the latest constituency values for each, instead of subselect?
 $links = $db->get_results('select basic.*, group_concat(tags_xref.tid) as tag_ids, group_concat(tags.name separator ", ") as tag_names from (select * from (select lc.entry, lc.id, date, text, href, content, constituency from link_constituency as lc
 left join links as l on (lc.entry = l.entry and lc.id = l.id)
@@ -36,17 +38,34 @@ group by basic.entry, basic.id');
 </style>
 <ol>
 <?php foreach ($links as $link):
+	$link_tags = explode(',', $link->tag_ids);
+
 	// If subword, skip.
-	if ( in_array( '14', explode(',', $link->tag_ids) ) )
+	if ( in_array( '14', $link_tags ) )
 		continue;
 
 	// If nonlinguistic, skip.
-	if ( in_array( '12', explode(',', $link->tag_ids) ) )
+	if ( in_array( '12', $link_tags ) )
 		continue;
+
+	// If across multiple sentences, skip.
+	if ( in_array( '15', $link_tags ) )
+		continue;
+
+	$tag_labels = join(' ', array_map(function($tag) {
+		global $tags;
+		$tag = intval($tag);
+		if ( !isset($tags[$tag]) )
+			return false;
+		if ( $tags[$tag]->constituency_specific != 'not_constituent' )
+			return false;
+		$label = $tags[(int) $tag]->name;
+		return "<span class='label'>$label</span>";
+	}, $link_tags));
 
 	$text = formatDisplayEntry(wp_kses_data($link->content), $link->text, str_replace('&', '&amp;', $link->href));
 ?>
-	<li><a href='display.php?entry=<?php echo (int) $link->entry; ?>&id=<?php echo (int) $link->id; ?>' data-placement='below' rel='twipsy' title='<?php echo esc_attr($link->tag_names); ?>'>#<?php echo (int) $link->entry; ?>:<?php echo (int) $link->id; ?></a>: <?php echo $text; ?></li>
+	<li><a href='display.php?entry=<?php echo (int) $link->entry; ?>&id=<?php echo (int) $link->id; ?>' data-placement='below' rel='twipsy' title='<?php echo esc_attr($link->tag_names); ?>'>#<?php echo (int) $link->entry; ?>:<?php echo (int) $link->id; ?></a>: <?php echo "$text $tag_labels"; ?></li>
 <?php endforeach; ?>
 </ol>
 <?php endif; ?>
